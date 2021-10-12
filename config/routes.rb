dd25a9adbf45a382fb57e6192d231ca8b1da0c8f
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-13 08:24:49 +0900

    ex

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   # get 'movies', to: 'movies#index'
   resources :reservations, only: [  :create]
-  scope :admin do
     get 'search', to: 'movies#search'
     resources :schedules, only: [:index]
     resources :movies do
       resources :schedules, except: [:index] do
         resources :sheets
         resources :reservations, except: [:create]
-      end
     end
   end
 end
