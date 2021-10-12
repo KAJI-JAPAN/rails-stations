bf06f8a1440d667c65ebeb72b82f4722ae2c2a8b
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-13 08:44:31 +0900

    ex

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,13 +8,12 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.new({name: reservation_params[:name], email: reservation_params[:email], date: reservation_params[:date], sheet_id: reservation_params[:sheet_id], schedule_id: reservation_params[:schedule_id]})
+    @reservation = Reservation.new(name: reservation_params[:name], email: reservation_params[:email], date: reservation_params[:date], sheet_id: reservation_params[:sheet_id], schedule_id: reservation_params[:schedule_id])
 
     if @reservation.save
       redirect_to movies_path
     else
-      # @movie_id = Movie.find(reservation_params[:schedule_id]).id
-      redirect_to movie_schedule_sheets_path(schedule_id: reservation_params[:schedule_id], movie_id: reservation_params[:movie_id], date: reservation_params[:date])
+      redirect_to movie_schedule_sheets_path(schedule_id: reservation_params[:schedule_id], movie_id: reservation_params[:movie_id], date: reservation_params[:date]), status: 400
     end
   end
 
