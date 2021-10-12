995dda115ac23d3b860670319a9dd1da89b32e58
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-12 18:01:43 +0900

    ex

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,7 +9,7 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new(reservation_params)
-    if @reservation.save
+    if @reservation.save && @reservation.valid?
       redirect_to movies_path
     else
       @movie_id = Movie.find(reservation_params[:schedule_id]).id
