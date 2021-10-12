e86b39fa7fd62a0c91734ff3344661c5b5f5f2e2
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-12 12:57:57 +0900

    ex

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,11 +9,11 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new(reservation_params)
-    if @reservation.save!
+    if @reservation.save
       redirect_to movies_path
     else
       @movie_id = Movie.find(reservation_params[:schedule_id]).id
-      redirect_to movie_schedule_sheets_path(schedule_id: reservation_params[:schedule_id], movie_id: @movie_id)
+      redirect_to movie_schedule_sheets_path(schedule_id: reservation_params[:schedule_id], movie_id: @movie_id, date: reservation_params[:date]), status: :bad_request
     end
   end
 
@@ -21,9 +21,4 @@ class ReservationsController < ApplicationController
   def reservation_params
     params.require(:reservation).permit(:name, :email, :date, :sheet_id, :schedule_id)
   end
-
-  # def redirect_params
-  #   params.require(:reservation).permit(:movie_id, :schedule_id)
-  # end
-
 end
