8c252fbcea1b3f084d39933496f16b0e7a82e209
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-13 21:38:51 +0900

    ex

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,7 @@ class Reservation < ApplicationRecord
   belongs_to :schedule
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :date, presence: true, uniqueness: {scppe: :sheet_id }
+  validates :date, presence: true, uniqueness: {scpoe: :sheet_id }
   validates :schedule_id, presence: true
-  validates :sheet_id, presence: true, uniqueness: {scppe: :data }
+  validates :sheet_id, presence: true, uniqueness: {scpoe: :data }
 end
