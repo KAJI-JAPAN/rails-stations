33d142a5129bb0db844d24824a60a9eb3ba1e76c
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-14 16:29:21 +0900

    ex

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,15 +2,15 @@ class ReservationsController < ApplicationController
   before_action :column_valid, only: [:new]
 
   def new
-    # if (params[:date] != nil) && (params[:sheet_id] != nil)
-      @reservation = Reservation.new
-      @movie_id = Movie.find(params[:movie_id])
-      @schedule_id = params[:schedule_id]
-      @date = params[:date]
-      @sheet_id = params[:sheet_id]
-    # else
-    #   render "movies/new", status: 400
-    # end
+    if (params[:date] == nil) || (params[:sheet_id] == nil)
+        render "movies/new", status: 400
+      else
+        @reservation = Reservation.new
+        @movie_id = Movie.find(params[:movie_id])
+        @schedule_id = params[:schedule_id]
+        @date = params[:date]
+        @sheet_id = params[:sheet_id]
+      end
   end
 
   def create
@@ -29,15 +29,8 @@ class ReservationsController < ApplicationController
     end
 
     def column_valid
-      if !params[:date] or !params[:sheet_id]
+      if params[:date] = nil or params[:sheet_id] != nil
         render "movies/new", status: 400
-      else
-        @reservation = Reservation.new
-        @movie_id = Movie.find(params[:movie_id])
-        @schedule_id = params[:schedule_id]
-        @date = params[:date]
-        @sheet_id = params[:sheet_id]
-        render status: 200  
       end
     end
 end
