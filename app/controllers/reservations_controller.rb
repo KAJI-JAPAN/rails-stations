276919527b095d08515be9d6719d4857805504a5
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-14 09:13:44 +0900

    ex

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -26,7 +26,7 @@ class ReservationsController < ApplicationController
 
     def column_valid
       if params[:date].nil? && params[:sheet_id].nil?
-        render template: "movies/index", status: 400
+        redirect_to movies_path
       end
     end
 end
