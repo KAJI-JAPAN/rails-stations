ab0a085fe943619bb64b9d87f998d056785dbde8
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-10-15 13:08:33 +0900

    ex

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,6 @@
 class ReservationsController < ApplicationController
-  # before_action :column_valid, only: [:new]
 
   def new
-    # if (params[:date] != nil) && (params[:sheet_id] != nil)
       @reservation = Reservation.new
       @movie_id = Movie.find(params[:movie_id])
       @schedule_id = params[:schedule_id]
@@ -29,10 +27,4 @@ class ReservationsController < ApplicationController
     def reservation_params
       params.require(:reservation).permit(:name, :email, :date, :sheet_id, :schedule_id, :movie_id)
     end
-
-    # def column_valid
-    #   if params[:date].nil? or params[:sheet_id].nil?
-    #     render "movies/new", status: 400
-    #   end
-    # end
 end
